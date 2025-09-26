26869d3181065adcf5886095672b144030de2d89
tirucloud <tirucloud@gmail.com>
2025-09-26 11:19:56 +0530

    aqa

---
+++ src/main/java/hello/SimpleHttpServer.java
@@ -0,0 +1,29 @@
+package hello;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // creates a default executor
+        System.out.println("Server started on port 8080");
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello from Java HTTP server!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
